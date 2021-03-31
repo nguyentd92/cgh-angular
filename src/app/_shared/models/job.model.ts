16b68bbad8d71d57b09a6807ba15d380fbfb80c2
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-31 11:44:40 +0700

    Day 2

---
+++ src/app/_shared/models/job.model.ts
@@ -0,0 +1,5 @@
+export class Job {
+  name!: string;
+  deadline!: Date;
+  doneAt?: Date;
+}
---
+++ src/app/task-item/task-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, OnInit, Output, HostBinding } from '@angular/core';
+import { Job } from '../_shared/models/job.model';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.scss']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() job: Job;
+  @Output() onDelete = new EventEmitter<Job>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @HostBinding('class.is-done') get isDone(): boolean {
+    return !!this.job.doneAt;
+  }
+
+  onClickDelete() {
+    this.onDelete.emit(this.job);
+  }
+
+  onChangeCheckDone($event) {
+    this.job.doneAt = $event ? new Date() : null;
+  }
+}
